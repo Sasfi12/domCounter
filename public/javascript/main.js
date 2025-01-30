4c6c7726ed7942094b7237bc586290521fb32307
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-01-30 11:18:11 +0100

    added colors

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -16,4 +16,17 @@ options.addEventListener("click", function(e) {
             console.log("HAHA ! you clicked on blanck !")
             break;
     }
+    console.log(result.textContent <"0")
+    if(result.textContent < "0") {
+        result.classList.add("red") 
+        result.classList.remove("green" , "black")
+    }
+    else if(result.textContent > "0") {
+        result.classList.add("green")
+        result.classList.remove("red" , "black")
+    }
+    else {
+        result.classList.add("black")
+        result.classList.remove("green" , "red")
+    }
 })
